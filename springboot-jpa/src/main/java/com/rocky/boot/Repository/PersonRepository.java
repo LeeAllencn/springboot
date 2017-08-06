81ab6508d8bc8a4ff48ddcc80ce01b1657966f67
lixin <lixin@example.com>
2017-08-06 23:33:12 +0800

    spring boot jpa

---
+++ springboot-jpa/src/main/java/com/rocky/boot/Repository/PersonRepository.java
@@ -0,0 +1,9 @@
+package com.rocky.boot.Repository;
+
+import com.rocky.boot.model.Person;
+
+/**
+ * Created by Rocky on 2017/8/6.
+ */
+public interface PersonRepository extends CustomRepository<Person, Long> {
+}
