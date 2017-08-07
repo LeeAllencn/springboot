3139193802cf2ee3c170e6cff9f16cd974e32261
lixin <lixin@hnair.com>
2017-08-07 20:11:47 +0800

    springboot-jpa

--- springboot-jpa/src/main/java/com/rocky/boot/Repository/PersonRepository.java
+++ springboot-jpa/src/main/java/com/rocky/boot/Repository/PersonRepository.java
@@ -1,9 +1,27 @@
 package com.rocky.boot.Repository;
 
 import com.rocky.boot.model.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 /**
  * Created by Rocky on 2017/8/6.
  */
-public interface PersonRepository extends CustomRepository<Person, Long> {
+public interface PersonRepository extends JpaRepository<Person, Long> {
+
+    //1 使用方法名查询，接受一个name参数
+    List<Person> findByAddress(String name);
+
+    //2 使用方法名查询，接受name和address参数
+    Person findByNameAndAddress(String name,String address);
+
+    //3 使用@Query查询，参数按照名称绑定
+    @Query("select p from Person p where p.name= :name and p.address= :address")
+    Person withNameAndAddressQuery(@Param("name")String name, @Param("address")String address);
+
+    //4 使用@NamedQuery查询，注意在实体类中做@NamedQuery的定义
+    Person withNameAndAddressNamedQuery(String name,String address);
 }
--- springboot-jpa/src/main/java/com/rocky/boot/model/Person.java
+++ springboot-jpa/src/main/java/com/rocky/boot/model/Person.java
@@ -5,62 +5,56 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.NamedQuery;
 
-/**
- * Created by Rocky on 2017/8/6.
- */
-@Entity //指明这是一个和数据库表映射的实体类
-@NamedQuery(name = "Person.withNameAndAddressNamedQuery", query = "select p from Person p where p.name=?1 and address=?2")
+@Entity //1 指明这是一个和数据库表映射的实体类
+@NamedQuery(name = "Person.withNameAndAddressNamedQuery",
+query = "select p from Person p where p.name=?1 and address=?2")
 public class Person {
+	@Id //2 主键
+	@GeneratedValue //3 默认使用主键的方式自增
+	private Long id;
+	
+	private String name;
+	
+	private Integer age;
+	
+	private String address;
+	
+	
+	
+	public Person() {
+		super();
+	}
+	public Person(Long id, String name, Integer age, String address) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.age = age;
+		this.address = address;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
 
-    @Id //数据库的主键
-    @GeneratedValue //使用主键生成方式为自增
-    private Long id;
 
-    private String name;
-
-    private Integer age;
-
-    private String address;
-
-    public Person() {
-    }
-
-    public Person(Long id, String name, Integer age, String address) {
-        this.id = id;
-        this.name = name;
-        this.age = age;
-        this.address = address;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
 }
