c2f7b98f9275bd7fce5ef0b1d11c4175a89fc616
lixin <lixin@example.com>
2017-08-08 22:17:45 +0800

    springboot-websocket

---
+++ springboot-websocket/src/main/java/com/rocky/boot/model/WiselyMessage.java
@@ -0,0 +1,9 @@
+package com.rocky.boot.model;
+
+public class WiselyMessage {
+    private String name;
+
+    public String getName(){
+        return name;
+    }
+}
---
+++ springboot-websocket/src/main/java/com/rocky/boot/model/WiselyResponse.java
@@ -0,0 +1,11 @@
+package com.rocky.boot.model;
+
+public class WiselyResponse {
+    private String responseMessage;
+    public WiselyResponse(String responseMessage){
+        this.responseMessage = responseMessage;
+    }
+    public String getResponseMessage(){
+        return responseMessage;
+    }
+}
