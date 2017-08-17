273c73adc8ced466c121b16dad31cfef89f5b2f1
lixin <lixin@hnair.com>
2017-08-17 18:17:37 +0800

    springboot-upload-file

---
+++ springboot-upload-file/src/main/java/com/rocky/boot/support/exception/StorageException.java
@@ -0,0 +1,12 @@
+package com.rocky.boot.support.exception;
+
+public class StorageException extends RuntimeException {
+
+    public StorageException(String message) {
+        super(message);
+    }
+
+    public StorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
