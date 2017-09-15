cc0b3e4e03e09d14895da525c0f6f8d8fcd8fba2
lixin <lixin@hnair.com>
2017-09-15 18:02:57 +0800

    jpa service update

--- springboot-jpa/src/test/java/com/rocky/boot/SpringbootJpaApplicationTests.java
+++ springboot-jpa/src/test/java/com/rocky/boot/SpringbootJpaApplicationTests.java
@@ -1,7 +1,10 @@
 package com.rocky.boot;
 
+import com.rocky.boot.Repository.PersonRepository;
+import com.rocky.boot.model.Person;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +12,24 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class SpringbootJpaApplicationTests {
 
+	@Autowired
+	private PersonRepository personRepository;
+
 	@Test
 	public void contextLoads() {
 	}
 
+	// 基本查询
+	@Test
+	public void testBaseQuery() throws Exception {
+		Person person=new Person(8L,"TT",30,"海口");
+		personRepository.findAll();
+		personRepository.findOne(8L);
+		personRepository.save(person);
+		personRepository.delete(person);
+		personRepository.count();
+		personRepository.exists(8L);
+		personRepository.
+		// ...
+	}
 }
