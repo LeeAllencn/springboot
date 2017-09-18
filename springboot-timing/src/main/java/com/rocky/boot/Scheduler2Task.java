193d68e13d026f441ba85c000e9e95d7fa57a8fe
lixin <lixin@hnair.com>
2017-09-18 15:53:40 +0800

    timing service

---
+++ springboot-timing/src/main/java/com/rocky/boot/Scheduler2Task.java
@@ -0,0 +1,21 @@
+package com.rocky.boot;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by Rocky on 2017-09-18.
+ */
+@Component
+public class Scheduler2Task {
+
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+
+    @Scheduled(fixedRate = 6000)
+    public void reportCurrentTime() {
+        System.out.println("现在时间：" + dateFormat.format(new Date()));
+    }
+}
---
+++ springboot-timing/src/main/java/com/rocky/boot/SpringbootTimingApplication.java
@@ -0,0 +1,14 @@
+package com.rocky.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class SpringbootTimingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTimingApplication.class, args);
+	}
+}
