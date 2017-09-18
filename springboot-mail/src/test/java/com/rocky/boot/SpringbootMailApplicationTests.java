7129703f1b995088b232566f459e61b44d4a8fb0
lixin <lixin@example.com>
2017-09-18 22:53:45 +0800

    mail service

--- springboot-mail/src/test/java/com/rocky/boot/SpringbootMailApplicationTests.java
+++ springboot-mail/src/test/java/com/rocky/boot/SpringbootMailApplicationTests.java
@@ -19,6 +19,34 @@ public class SpringbootMailApplicationTests {
 		mailService.sendSimpleMail("13342437205@163.com","test simple mail"," hello this is simple mail");
 	}
 
+	//TODO
+	@Test
+	public void testHtmlMail() throws Exception {
+		String content="<html>\n" +
+				"<body>\n" +
+				"    <h3>hello world ! 这是一封Html邮件!</h3>\n" +
+				"</body>\n" +
+				"</html>";
+		mailService.sendHtmlMail("13342437205@163.com","test simple mail",content);
+	}
+
+	//TODO
+	@Test
+	public void sendAttachmentsMail() {
+		String filePath= "images/test.jpg";
+		mailService.sendAttachmentsMail("13342437205@163.com", "主题：带附件的邮件", "有附件，请查收！", filePath);
+	}
+
+	//TODO
+	@Test
+	public void sendInlineResourceMail() {
+		String rscId = "lee006";
+		String content="<html><body>这是有图片的邮件：<img src=\'cid:" + rscId + "\' ></body></html>";
+		String imgPath = "images/test.jpg";
+
+		mailService.sendInlineResourceMail("13342437205@163.com", "主题：这是有图片的邮件", content, imgPath, rscId);
+	}
+
 	@Test
 	public void contextLoads() {
 	}
