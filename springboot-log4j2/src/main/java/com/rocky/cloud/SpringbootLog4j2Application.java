5a7466435df5bb6dccc9ae1ecdc99a194377b420
lixin <lixin@hnair.com>
2017-10-26 14:45:29 +0800

    log4j2

---
+++ springboot-log4j2/src/main/java/com/rocky/cloud/SpringbootLog4j2Application.java
@@ -0,0 +1,12 @@
+package com.rocky.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLog4j2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootLog4j2Application.class, args);
+	}
+}
