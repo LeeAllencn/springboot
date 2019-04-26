6bc29a001769ee5748b8057349dfabf8a44d9398
lixin <1137317886@qq.com>
2019-04-26 19:39:47 +0800

    activiti

---
+++ springboot-activiti/src/main/java/com/rocky/boot/springbootactiviti/SpringbootActivitiApplication.java
@@ -0,0 +1,13 @@
+package com.rocky.boot.springbootactiviti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootActivitiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootActivitiApplication.class, args);
+    }
+
+}
