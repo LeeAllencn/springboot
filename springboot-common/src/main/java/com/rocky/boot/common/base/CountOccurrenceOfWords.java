75ef05a792b33f56a947cceaf164ae8850c4093c
Rocky <1137317886@qq.com>
2021-02-09 11:29:43 +0800

    common

---
+++ springboot-common/src/main/java/com/rocky/boot/common/base/CountOccurrenceOfWords.java
@@ -0,0 +1,38 @@
+package com.rocky.boot.common.base;
+
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
+/**
+ * @Author: Rocky
+ * @Description: 统计单词的出现次数
+ * @Date: Created in 13:03 2018/6/23
+ */
+public class CountOccurrenceOfWords {
+
+    public static void main(String[] args) {
+        String text = "Good morning.Have a good class.Have a good visit.Have fun!";
+
+        TreeMap<String, Integer> treeMap = new TreeMap<>();
+
+        String[] words = text.split("[ \n\t\r.,;:!?(){}]");
+        for (int i = 0; i < words.length; i++) {
+            String key = words[i].toLowerCase();
+            if (key.length() > 0) {
+                if (treeMap.get(key) == null) {
+                    treeMap.put(key, 1);
+                } else {
+                    int value = treeMap.get(key).intValue();
+                    value++;
+                    treeMap.put(key, value);
+                }
+            }
+        }
+
+        Set<Map.Entry<String, Integer>> entrySet = treeMap.entrySet();
+        for (Map.Entry<String, Integer> entry : entrySet) {
+            System.out.println(entry.getKey() + "\t" + entry.getValue());
+        }
+    }
+}
---
+++ springboot-common/src/main/java/com/rocky/boot/common/utils/FloatingOperationUtil.java
@@ -0,0 +1,85 @@
+package com.rocky.boot.common.utils;
+
+import java.math.BigDecimal;
+
+/**
+ * @Author: Rocky
+ * @Description: 浮点数的基本运算
+ * 注：
+ * 1.通常建议优先使用基于String参数的构造器来创建BigDecimal对象
+ * 2.不要直接使用double浮点数参数的构造器来创建BigDecimal对象，同样会发生精度丢失的问题
+ * 3.通过BigDecimal.valueOf(double value)静态方法来创建BigDecimal对象
+ * @Date: Created in 13:39 2018/6/23
+ */
+public class FloatingOperationUtil {
+
+    /**
+     * 默认除法运算精度
+     */
+    private static final int DEF_DIV_SCALE = 10;
+
+    /**
+     * 构造器私有，让这个类不能实例化
+     */
+    private FloatingOperationUtil() {
+    }
+
+    /**
+     * 提供精确的加法运算:add()
+     *
+     * @param v1
+     * @param v2
+     * @return
+     */
+    public static double add(double v1, double v2) {
+        BigDecimal b1 = BigDecimal.valueOf(v1);
+        BigDecimal b2 = BigDecimal.valueOf(v2);
+        return b1.add(b2).doubleValue();
+    }
+
+    /**
+     * 提供精确的减法运算:subtract()
+     *
+     * @param v1
+     * @param v2
+     * @return
+     */
+    public static double sub(double v1, double v2) {
+        BigDecimal b1 = BigDecimal.valueOf(v1);
+        BigDecimal b2 = BigDecimal.valueOf(v2);
+        return b1.subtract(b2).doubleValue();
+    }
+
+    /**
+     * 提供精确的乘法运算:multiply()
+     *
+     * @param v1
+     * @param v2
+     * @return
+     */
+    public static double mul(double v1, double v2) {
+        BigDecimal b1 = BigDecimal.valueOf(v1);
+        BigDecimal b2 = BigDecimal.valueOf(v2);
+        return b1.multiply(b2).doubleValue();
+    }
+
+    /**
+     * 提供精确的除法运算:divide()
+     *
+     * @param v1
+     * @param v2
+     * @return
+     */
+    public static double div(double v1, double v2) {
+        BigDecimal b1 = BigDecimal.valueOf(v1);
+        BigDecimal b2 = BigDecimal.valueOf(v2);
+        return b1.divide(b2, DEF_DIV_SCALE, BigDecimal.ROUND_HALF_UP).doubleValue();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(add(0.05, 0.01));
+        System.out.println(sub(1.0, 0.42));
+        System.out.println(mul(4.015, 100));
+        System.out.println(div(123.3, 100));
+    }
+}
