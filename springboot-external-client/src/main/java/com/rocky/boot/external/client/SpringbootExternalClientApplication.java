bc3a5941a210866eec4c400881002caefb2a9b12
Rocky <1137317886@qq.com>
2021-03-11 15:10:36 +0800

    openapi and external:init

---
+++ springboot-external-client/src/main/java/com/rocky/boot/external/client/SpringbootExternalClientApplication.java
@@ -0,0 +1,13 @@
+package com.rocky.boot.external.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootExternalClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootExternalClientApplication.class, args);
+    }
+
+}
---
+++ springboot-openapi-service/src/main/java/com/rocky/boot/openapi/SpringbootOpenapiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rocky.boot.openapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootOpenapiServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootOpenapiServiceApplication.class, args);
+    }
+
+}
