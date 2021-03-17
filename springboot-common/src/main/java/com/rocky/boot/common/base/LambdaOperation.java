a8f29da957d92f68119d6fd50650ec73cf8fd10d
Rocky <1137317886@qq.com>
2021-03-17 19:58:23 +0800

    common

---
+++ springboot-common/src/main/java/com/rocky/boot/common/base/LambdaOperation.java
@@ -0,0 +1,80 @@
+package com.rocky.boot.common.base;
+
+import java.util.Arrays;
+import java.util.IntSummaryStatistics;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+/**
+ * @author rocky
+ * Description: Lambda 表达式
+ * 语法:
+ * (parameters) -> expression
+ * (parameters) ->{ statements; }
+ * <p>
+ * Created in 2021/3/17
+ */
+public class LambdaOperation {
+
+    public static void filter(List names, Predicate condition) {
+        names.stream().filter((name) -> (condition.test(name))).forEach(System.out::println);
+    }
+
+    public static void main(String[] args) {
+
+        // example1 使用lambda表达式对列表进行迭代
+        List<String> features = Arrays.asList("Lambdas", "Default Method", "Stream API", "Date and Time API");
+        features.forEach(System.out::println);
+
+        // example2 使用lambda表达式和函数式接口Predicate
+        List<String> languages = Arrays.asList("Java", "Scala", "C++", "Haskell", "Lisp");
+
+        System.out.println("Languages which starts with J :");
+        filter(languages, (Predicate<String>) (str) -> str.startsWith("J"));
+
+        System.out.println("Languages which ends with a ");
+        filter(languages, (Predicate<String>) (str) -> str.endsWith("a"));
+
+        System.out.println("Print all languages :");
+        filter(languages, (str) -> true);
+
+        System.out.println("Print no language : ");
+        filter(languages, (str) -> false);
+
+        System.out.println("Print language whose length greater than 4:");
+        filter(languages, (Predicate<String>) (str) -> str.length() > 4);
+
+        // example3 使用lambda表达式的Map和Reduce示例
+        List<Integer> costBeforeTax = Arrays.asList(100, 200, 300, 400, 500);
+        costBeforeTax.stream().map((cost) -> cost + .12 * cost).forEach(System.out::println);
+        Optional<Double> total = costBeforeTax.stream().map((cost) -> cost + .12 * cost).reduce(Double::sum);
+        total.ifPresent(aDouble -> System.out.println("Total : " + aDouble));
+
+        // example4 通过过滤创建一个String列表
+        List<String> strList = Arrays.asList("abc", "", "bcd", "", "defg", "jk");
+        List<String> filtered = strList.stream().filter(x -> x.length() > 2).collect(Collectors.toList());
+        System.out.printf("Original List : %s, filtered list : %s %n", strList, filtered);
+
+        // example5 对列表的每个元素应用函数
+        List<String> g7 = Arrays.asList("USA", "Japan", "France", "Germany", "Italy", "U.K.", "Canada");
+        String g7Countries = g7.stream().map(String::toUpperCase).collect(Collectors.joining(", "));
+        System.out.println(g7Countries);
+
+        // example6 复制不同的值，创建一个子列表
+        List<Integer> numbers = Arrays.asList(9, 10, 3, 4, 7, 3, 4);
+        List<Integer> distinct = numbers.stream().map(i -> i * i).distinct().collect(Collectors.toList());
+        System.out.printf("Original List : %s,  Square Without duplicates : %s %n", numbers, distinct);
+
+        // example7  计算集合元素的最大值、最小值、总和以及平均值
+        List<Integer> primes = Arrays.asList(2, 3, 5, 7, 11, 13, 17, 19, 23, 29);
+        IntSummaryStatistics intSummaryStatistics = primes.stream().mapToInt(x -> x).summaryStatistics();
+        System.out.println("Highest prime number in List : " + intSummaryStatistics.getMax());
+        System.out.println("Lowest prime number in List : " + intSummaryStatistics.getMin());
+        System.out.println("Sum of all prime numbers : " + intSummaryStatistics.getSum());
+        System.out.println("Average of all prime numbers : " + intSummaryStatistics.getAverage());
+    }
+
+
+}
---
+++ springboot-common/src/main/java/com/rocky/boot/common/utils/GrpcBeanUtils.java
@@ -0,0 +1,57 @@
+package com.rocky.boot.common.utils;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.protobuf.Message;
+import com.google.protobuf.util.JsonFormat;
+
+import java.io.IOException;
+
+/**
+ * @author rocky
+ * Description: Grpc对象和java bean的复制
+ * Created in 2021/3/17
+ */
+public class GrpcBeanUtils {
+
+    private static final String DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
+
+    /**
+     * 将java bean对象拷贝到grpc对象
+     *
+     * @param sourcePojoBean java bean对象
+     * @param destBuilder    grpc对象
+     * @throws IOException IOException
+     */
+    public static void toProtoBean(Object sourcePojoBean, Message.Builder destBuilder) throws IOException {
+        if (sourcePojoBean == null) {
+            throw new IllegalArgumentException("No source pojo specified");
+        }
+        if (destBuilder == null) {
+            throw new IllegalArgumentException("No destination message builder specified");
+        }
+
+        String json = new GsonBuilder().setDateFormat(DATE_TIME_FORMAT).create().toJson(sourcePojoBean);
+        JsonFormat.parser().ignoringUnknownFields().merge(json, destBuilder);
+    }
+
+    /**
+     * 将grpc对象拷贝到java bean对象
+     *
+     * @param sourceMessage grpc对象
+     * @param destPojoClass java bean对象
+     * @param <T>           clazz
+     * @return T
+     * @throws IOException IOException
+     */
+    public static <T> T toPojoBean(Message sourceMessage, Class<T> destPojoClass) throws IOException {
+        if (sourceMessage == null) {
+            throw new IllegalArgumentException("No source message specified");
+        }
+        if (destPojoClass == null) {
+            throw new IllegalArgumentException("No destination pojo class specified");
+        }
+        String json = JsonFormat.printer().print(sourceMessage);
+        return new Gson().fromJson(json, destPojoClass);
+    }
+}
