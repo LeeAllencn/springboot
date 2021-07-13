85a906e3ed7a5ec58a6cbd24cd66db8cc952babf
Rocky <1137317886@qq.com>
2021-07-13 21:05:40 +0800

    common:thread

--- springboot-common/src/main/java/com/rocky/boot/common/base/thread/CallableTest.java
+++ springboot-common/src/main/java/com/rocky/boot/common/base/thread/CallableTest.java
@@ -1,11 +1,11 @@
 package com.rocky.boot.common.base.thread;
 
+import com.google.common.util.concurrent.*;
 import com.rocky.boot.common.utils.ThreadPoolUtil;
+import org.checkerframework.checker.nullness.qual.Nullable;
+import org.jetbrains.annotations.NotNull;
 
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.FutureTask;
-import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.*;
 
 /**
  * @author : rocky
@@ -26,6 +26,8 @@ public class CallableTest implements Callable<Integer> {
     }
 
     public static void main(String[] args) {
+
+        // 使用方式一：FutureTask
         CallableTest callableTest = new CallableTest();
         FutureTask<Integer> futureTask = new FutureTask<>(callableTest);
         ThreadPoolExecutor threadPoolExecutor = ThreadPoolUtil.createThreadPool(CallableTest.class);
@@ -36,5 +38,28 @@ public class CallableTest implements Callable<Integer> {
         } catch (InterruptedException | ExecutionException e) {
             e.printStackTrace();
         }
+
+        // 使用方式二：ListenableFuture
+        // 创建一个线程池service
+        ExecutorService delegate = ThreadPoolUtil.createThreadPool(CallableTest.class);
+        // 创建一个ListeningExecutorService实例
+        ListeningExecutorService executorService = MoreExecutors.listeningDecorator(delegate);
+        // 提交一个可监听的线程
+        ListenableFuture<Integer> listenableFuture = executorService.submit(new CallableTest());
+        // 线程结果处理回调函数
+        Futures.addCallback(listenableFuture, new FutureCallback<Integer>() {
+
+            @Override
+            public void onSuccess(@Nullable Integer result) {
+                System.out.println("ListenableFutureTest return:" + result);
+            }
+
+            @Override
+            public void onFailure(@NotNull Throwable t) {
+                System.out.println("ListenableFutureTest failed:" + t.getMessage());
+            }
+        } , executorService);
+
+        executorService.shutdown();
     }
 }
